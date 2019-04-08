8984033b5846fe7e00445f5ade4945c1f81bbad9
bear <bear@weedo.hk>
2019-04-08 19:57:12 +0800

    update

---
+++ sql/in_app_purchase_users.sql
@@ -0,0 +1,24 @@
+SELECT
+  user_pseudo_id, event_params.value.string_value as product_name
+FROM
+  `analytics_195246954.events_*` AS T,
+  T.event_params
+WHERE
+  event_name = 'in_app_purchase'
+  AND event_params.key = 'product_name'
+  AND geo.country = 'United States'
+  AND platform = '{0}'
+  AND _TABLE_SUFFIX BETWEEN '{1}'
+  AND '{2}'
+  AND user_pseudo_id IN (
+  SELECT
+    DISTINCT user_pseudo_id
+  FROM
+    `analytics_195246954.events_*` AS T,
+    T.event_params
+  WHERE
+    event_name = 'first_open'
+    AND geo.country = 'United States'
+    AND platform = '{0}'
+    AND _TABLE_SUFFIX BETWEEN '{1}'
+    AND '{1}' )
